02fcdab16933f0f660809440b891e62907e78a9d
BuildTools <lStewieAl@gmail.com>
2020-01-13 15:44:23 +0000

    Release 0.28

--- ZeLogWindow.h
+++ ZeLogWindow.h
@@ -617,7 +617,7 @@ LRESULT CALLBACK EditorUI_WndProc(HWND Hwnd, UINT Message, WPARAM wParam, LPARAM
 	{
 		//	Continue normal execution but with a custom string
 		char customTitle[256];
-		sprintf_s(customTitle, "%s -= GECK Extender Rev. 0.27 =-", (const char *)lParam);
+		sprintf_s(customTitle, "%s -= GECK Extender Rev. 0.28 =-", (const char *)lParam);
 
 		return CallWindowProc(OldEditorUI_WndProc, Hwnd, Message, wParam, (LPARAM)customTitle);
 	}
