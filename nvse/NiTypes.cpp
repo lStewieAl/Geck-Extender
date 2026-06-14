bddefcc4a33fc18da31ee179ac19312bdb38a8e5
lStewieAl <lStewieAl@gmail.com>
2026-06-14 12:55:58 +0100

    Decoding

--- nvse/NiTypes.cpp
+++ nvse/NiTypes.cpp
@@ -1,6 +1,15 @@
 #include "NiTypes.h"
-NiPoint3 const NiPoint3::UNIT_X = NiPoint3(1,0,0);
-NiPoint3 const NiPoint3::UNIT_Y = NiPoint3(0,1,0);
-NiPoint3 const NiPoint3::UNIT_Z = NiPoint3(0,0,1);
-NiPoint3 const NiPoint3::UNIT_ALL = NiPoint3(1,1,1);
-NiPoint3 const NiPoint3::ZERO = NiPoint3(0,0,0);
+const NiPoint3 NiPoint3::UNIT_X = NiPoint3(1,0,0);
+const NiPoint3 NiPoint3::UNIT_Y = NiPoint3(0,1,0);
+const NiPoint3 NiPoint3::UNIT_Z = NiPoint3(0,0,1);
+const NiPoint3 NiPoint3::UNIT_ALL = NiPoint3(1,1,1);
+const NiPoint3 NiPoint3::ZERO = NiPoint3(0,0,0);
+
+const NiColorA NiColorA::WHITE = NiColorA(1, 1, 1);
+const NiColorA NiColorA::RED = NiColorA(1, 0, 0);
+const NiColorA NiColorA::GREEN = NiColorA(0, 1, 0);
+const NiColorA NiColorA::BLUE = NiColorA(0, 0, 1);
+const NiColorA NiColorA::CYAN = NiColorA(0, 1, 1);
+const NiColorA NiColorA::MAGENTA = NiColorA(1, 0, 1);
+const NiColorA NiColorA::YELLOW = NiColorA(1, 1, 0);
+const NiColorA NiColorA::BLACK = NiColorA(0, 0, 0);
